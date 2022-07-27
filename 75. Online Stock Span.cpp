36b01e4c1ba677b549102b072a0685c136a8d347
Sagar Srivastava <57001832+Sagar-Srivastava-2001@users.noreply.github.com>
2022-07-27 11:49:35 +0530

    Create 75. Online Stock Span.cpp

---
+++ 75. Online Stock Span.cpp
@@ -0,0 +1,25 @@
+class StockSpanner {
+public:
+    stack<pair<int,int>> s;     // {price, count}
+    
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int count = 0;
+        while(!s.empty() and s.top().first <= price){
+            count += s.top().second;
+            s.pop();
+        }
+        count++;
+        s.push({price, count});
+        return count;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */

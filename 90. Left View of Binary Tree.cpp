1b558390b889d3402bd4fb8283ee1d34d287854d
Sagar Srivastava <57001832+Sagar-Srivastava-2001@users.noreply.github.com>
2022-07-27 12:02:12 +0530

    Update 90. Left View of Binary Tree.cpp

--- 90. Left View of Binary Tree.cpp
+++ 90. Left View of Binary Tree.cpp
@@ -1,43 +1,28 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> preorder;
-        TreeNode* curr = root;
-        
-        while(curr != NULL){
-            if(curr->left == NULL){
-                preorder.push_back(curr->val);
-                curr = curr->right;
+vector<int> leftView(Node *root)
+{
+    vector<int> leftview;
+    queue<Node*> q;
+    q.push(root);
+    
+    while(!q.empty()){
+        int size = q.size();
+        int i = 0;
+        for(int i=0;i<size;i++){
+            Node* temp = q.front();
+            q.pop();
+            
+            if(i == 0){
+                leftview.push_back(temp->data);
             }
-            else
-            {
-                TreeNode* prev = curr->left;
-                while(prev->right and prev->right != curr){
-                    prev = prev->right;
-                }
+            
+            if(temp->left)
+                q.push(temp->left);
                 
-                if(prev->right == NULL){
-                    prev->right = curr;
-                    preorder.push_back(curr->val);
-                    curr = curr->left;
-                }
-                else{
-                    prev->right = NULL;
-                    curr = curr->right;
-                }
-            }
+            if(temp->right)
+                q.push(temp->right);
+            
+            i++;
         }
-        return preorder;
     }
-};
+    return leftview;
+}
